39455bf3f7b3fd6574f2d0de32ab6baa57cdbf6d
ramanbhati <ramanbhati.10@gmail.com>
2015-09-09 15:50:06 -0500

    commented out some unused code in app.js

--- app.js
+++ app.js
@@ -40,177 +40,83 @@ tutorialApp.config(['$routeProvider', function ($routeProvider) {
           })
 }]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var serverUrl = 'http://127.0.0.1:1337/';
-testApp.factory('Raman', function($http, $q) {
-  return {
-    // getServerData: function (successCallBack) {
-    //   $http({
-    //     method: 'GET',
-    //     url: serverUrl
-    //   }).
-    //   success(function(data,status,header, config) {
-    //      successCallBack(data);
-    //   }).
-    //   error(function(data,status,header, config) {
-    //     console.log('Error');
-    //   })
-    // }
-
-    getServerDataWithPromises: function () {
-      var defer = $q.defer();
-      $http({
-        method: 'GET',
-        url: serverUrl
-      }).
-      success(function(data,status,header,config) {
-        defer.resolve(data);
-      }).
-      error(function(data,status,header,config){
-        defer.reject("Failed");
-      })
-
-      return defer.promise;
-    }
-
-
-
-  }
-});
-
-testApp.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.
-          when('/login', {
-            templateUrl: 'templates/login.html', 
-            controller: 'loginController'
-          }).
-          when('/home', {
-            templateUrl: 'templates/home.html', 
-            controller: 'homeController'
-          })
-
-}]);
-
-testApp.controller('homeController', function ($scope) {
-  $scope.home = {
-    msg: 'I am home'
-  }
-  console.log('I am home');
-});
-
-testApp.controller('loginController', function ($scope) {
-  $scope.login = {
-    msg: 'I am in login'
-  }
-  console.log('I am login');
-});
-
-testApp.controller('myHttpController', function ($scope, Raman, $log) {
-  var dataWithQService = Raman.getServerDataWithPromises();
-  dataWithQService.then(
-    function(data) {
-      $scope.serverData = data;
-    })
-  // Raman.getServerData(function(data) {
-  //   $scope.serverData = data;
-  // });
-});
-
-
-testApp.controller('testController', function ($scope, Raman) {
+// var serverUrl = 'http://127.0.0.1:1337/';
+// testApp.factory('Raman', function($http, $q) {
+//   return {
+//     // getServerData: function (successCallBack) {
+//     //   $http({
+//     //     method: 'GET',
+//     //     url: serverUrl
+//     //   }).
+//     //   success(function(data,status,header, config) {
+//     //      successCallBack(data);
+//     //   }).
+//     //   error(function(data,status,header, config) {
+//     //     console.log('Error');
+//     //   })
+//     // }
+
+//     getServerDataWithPromises: function () {
+//       var defer = $q.defer();
+//       $http({
+//         method: 'GET',
+//         url: serverUrl
+//       }).
+//       success(function(data,status,header,config) {
+//         defer.resolve(data);
+//       }).
+//       error(function(data,status,header,config){
+//         defer.reject("Failed");
+//       })
+
+//       return defer.promise;
+//     }
+
+
+
+//   }
+// });
+
+// testApp.config(['$routeProvider', function ($routeProvider) {
+//   $routeProvider.
+//           when('/login', {
+//             templateUrl: 'templates/login.html', 
+//             controller: 'loginController'
+//           }).
+//           when('/home', {
+//             templateUrl: 'templates/home.html', 
+//             controller: 'homeController'
+//           })
+
+// }]);
+
+// testApp.controller('homeController', function ($scope) {
+//   $scope.home = {
+//     msg: 'I am home'
+//   }
+//   console.log('I am home');
+// });
+
+// testApp.controller('loginController', function ($scope) {
+//   $scope.login = {
+//     msg: 'I am in login'
+//   }
+//   console.log('I am login');
+// });
+
+// testApp.controller('myHttpController', function ($scope, Raman, $log) {
+//   var dataWithQService = Raman.getServerDataWithPromises();
+//   dataWithQService.then(
+//     function(data) {
+//       $scope.serverData = data;
+//     })
+//   // Raman.getServerData(function(data) {
+//   //   $scope.serverData = data;
+//   // });
+// });
+
+
+// testApp.controller('testController', function ($scope, Raman) {
    
-   $scope.partial = "templates/registration.html";
-});
+//    $scope.partial = "templates/registration.html";
+// });
