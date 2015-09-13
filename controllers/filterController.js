ebdc603fed075e9d4fd808175603b5cace888132
ramanbhati <ramanbhati.10@gmail.com>
2015-09-13 01:40:41 -0500

    added more functionality in filterController

--- controllers/filterController.js
+++ controllers/filterController.js
@@ -1,5 +1,10 @@
 tutorialApp.controller('filterController', function($scope) {
   $scope.filterObject = {
+  	arrayExample : ['Ari', 'Lerner', 'Likes', 'To', 'Eat', 'Pizza'],
+  	jsonExample : {'name': 'Ari', 'City': 'San Francisco'},
+  	jsonFunc : function () {
+  		$scope.filterObject.json = 'json';
+  	}
   
   };
 });
